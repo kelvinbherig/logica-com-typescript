d4290f6ce507ce12a2d9ce9c9550a5223a2b0b14
Kelvinbherig <kelvinbherig9@gmail.com>
2026-05-12 13:45:25 -0300

    Update variaveis.ts

--- variaveis.ts
+++ variaveis.ts
@@ -1,1 +1,13 @@
+let contador:number = 0
+
+// operadores de atribuição =, +=, -=, *=
+
+contador = contador + 1
+contador += 1
+contador++
+console.log("Incremento", contador)
+contador -= 1
+console.log("Decremento", contador)
+contador *= 2
+console.log("Multiplicação", contador)
 
